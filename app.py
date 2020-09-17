d722b82d96ae7ec416d978c8c6105736b22dd57b
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-09-17 09:01:32 +0500

    Added delete and view single post functionality

--- app.py
+++ app.py
@@ -29,13 +29,38 @@ def new():
     author = request.form.get('author').capitalize()
     title = request.form.get('title').capitalize()
     content = request.form.get('content')
-
-    new_post = Post(author, title, content)
-    db.session.add(new_post)
-    db.session.commit()
-    return redirect("/")
+    if not author.strip() or not title.strip() or content.strip():
+      return redirect("/")
+    else:
+      new_post = Post(author, title, content)
+      db.session.add(new_post)
+      db.session.commit()
+      return redirect("/")
 
   return render_template('new.html')
+
+@app.route("/posts/<int:id>")
+def view(id):
+  try:
+    post = Post.query.filter_by(_id=id).first()
+  except Exception as e:
+    print(e)
+  else:
+    if not post:
+      return redirect("/")
+    else:
+      return render_template("post.html", post=post)
+
+@app.route("/delete/<int:id>")
+def deletePost(id):
+  try:
+    post = Post.query.filter_by(_id=id).first()
+    db.session.delete(post)
+    db.session.commit()
+  except Exception as e:
+    print(e)
+  finally:
+    return redirect("/")
 
 if __name__ == '__main__':
   db.create_all()
